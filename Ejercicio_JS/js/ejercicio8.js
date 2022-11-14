8321c8abc86366a888ffa915382bcebfa6483451
Angel <efox.host@gmail.com>
2022-11-14 20:30:51 +0100

    Ejercicios JS 8

--- Ejercicio_JS/js/ejercicio8.js
+++ Ejercicio_JS/js/ejercicio8.js
@@ -1,8 +1,11 @@
-
-function() {
-    suma(12, 12);
-}
-
 function suma(param1, param2){
-    console.log(param1 + param2);
+    alert(param1 + param2);
+    document.getElementById("resultado").innerHTML=param1+param2;
+    console.log(document.getElementById("entrada").innerHTML);
+    return console.log(param1 + param2);
 }
+
+(function (param1, param2) {
+    alert(param1 + param2);
+    console.log(param1 + param2);
+}(12, 12));
