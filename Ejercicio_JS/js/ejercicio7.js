ec5d72d510504cb3dc18a2cda6b383c2c09063dd
Angel <efox.host@gmail.com>
2022-11-14 19:51:52 +0100

    Ejercicio JS 7

---
+++ Ejercicio_JS/js/ejercicio7.js
@@ -0,0 +1,21 @@
+let pieza1 = {
+    peso: 20
+};
+
+let pieza2 = {
+    peso: '20'
+};
+
+if(pieza1.peso == pieza2.peso){
+    alert("Son iguales");
+} else {
+    alert("No son iguales");
+}
+
+if(pieza1.peso === pieza2.peso){
+    alert("Son iguales");
+} else {
+    alert("No son iguales");
+}
+
+pieza1.peso == 20 ? (alert(pieza1.peso + pieza2.peso)) : (alert(pieza1.peso * pieza2.peso));
