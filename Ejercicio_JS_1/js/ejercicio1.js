bb7f968ee1c4173fa39f2b29d2ea67e1e1bc0118
Angel <efox.host@gmail.com>
2022-11-14 15:42:55 +0100

    Ejercicio JS

---
+++ Ejercicio_JS_1/js/ejercicio1.js
@@ -0,0 +1,45 @@
+let message = "MENSAJE DE ";
+
+console.info(message+"INFO");
+console.warn(message+"WARN");
+console.error(message+"ERROR");
+console.log(message+"LOG");
+console.time();
+console.timeEnd();
+
+/* Extensible */
+let numero_1 = 2;
+let numero_2 = 34;
+let resultado = 0;
+
+console.log("Suma: " + calculos(numero_1, numero_2, "+") );
+console.log("Resta: " + calculos(numero_1, numero_2, "-") );
+
+function calculos(valor_1, valor_2, simbolo){
+    switch(simbolo){
+    case "+":
+        resultado = valor_1 + valor_2;
+        break;
+    case "-":
+        if(valor_1 < valor_2){
+            resultado = valor_2 - valor_1; 
+        } else {
+            resultado = valor_1 - valor_2; 
+        }
+        break;
+    }    
+    return resultado;
+}
+
+/* Objeto */
+let pc = {
+    name : 'Windows',
+    year: 1995
+};
+console.log(pc['name'] + " / " + pc['year']);
+
+/* Array de objeto */
+let listaObjetos = ["Manolo","Mario","Carlos"];
+for(let x = 0; x < listaObjetos.length; x++){
+    console.log(listaObjetos[x]);
+}
