bc3151e47aede52a56747a0ee080c9357d5c2080
Angel <efox.host@gmail.com>
2022-11-15 16:31:22 +0100

    Ejercicio JS 9

--- Ejercicio_JS/js/ejercicio_9_funciones.js
+++ Ejercicio_JS/js/ejercicio_9_funciones.js
@@ -1,3 +1,18 @@
+/*
+a) Define en el HTML 2 divs que contengan 1 número cada uno, otro div vacío y un botón que llame a la función d).
+
+b) Define una función que reciba 2 parámetros y devuelva el resultado de la suma.
+
+c) Define una función que reciba 2 parámetros y devuelva el resultado de la multiplicación.
+
+d) Define una función que recoja los 2 parámetros definidos en los divs del apartado 
+	a), los sume con la función 
+	b) y los multiplique con la función c).
+
+    A continuación y en esa misma función, se deberán sumar los 2 resultados obtenidos de 
+    las funciones b) y c) y representar el resultado en el 3º div definido en el html.
+*/
+
 function suma(parametro_uno, parametro_dos) {
     return parametro_uno + parametro_dos;
 }
