72eea31a5b0f5b492e41eb839b928c0eea5158a0
Angel <efox.host@gmail.com>
2022-11-15 20:11:36 +0100

    Ejercicio Fecha JS

--- Ejercicio_JS/js/ejercicioFecha.js
+++ Ejercicio_JS/js/ejercicioFecha.js
@@ -17,6 +17,7 @@ function fecha (fechaActual, ampliarMeses){
     if(mes < 10){ mes = "0" + mes; }
     if(dia < 10){ dia = "0" + dia; }
 
-    // Mostramos en consola el resultado final
-    console.log(dia + "/" + mes + "/" + anyo);
+    
+    console.log(dia + "/" + mes + "/" + anyo);  // Mostramos en consola el resultado final
+    alert(dia + "/" + mes + "/" + anyo);        // Mostramos en alerta el resultado final
 }
