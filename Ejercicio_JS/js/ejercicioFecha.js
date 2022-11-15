5046b881a068db8b03537477bc2a43f35f6d6eb5
Angel <efox.host@gmail.com>
2022-11-15 20:09:41 +0100

    Ejercicio Fecha JS

---
+++ Ejercicio_JS/js/ejercicioFecha.js
@@ -0,0 +1,22 @@
+
+function fecha (fechaActual, ampliarMeses){
+    let fechaCadena = fechaActual.split("-");
+    let suma = Number(fechaCadena[1]) + Number(ampliarMeses);
+
+    let anyo = Number(fechaCadena[0]);
+    let mes  = Number(fechaCadena[1]);
+    let dia  = Number(fechaCadena[2]);
+
+    // Total de meses + Meses agregados, si se supera sumar al año y restar al mes
+    if(suma > 12){ 
+        anyo = Number(fechaCadena[0]) + 1;
+        mes = (Number(fechaCadena[1]) + ampliarMeses) - 12;
+    }
+
+    // Sí el mes es menor a 10 agregarle un cero delante
+    if(mes < 10){ mes = "0" + mes; }
+    if(dia < 10){ dia = "0" + dia; }
+
+    // Mostramos en consola el resultado final
+    console.log(dia + "/" + mes + "/" + anyo);
+}
