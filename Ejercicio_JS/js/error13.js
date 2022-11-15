7f7874089a4346b80bac81a82ba63bb004c6329c
Angel <efox.host@gmail.com>
2022-11-15 19:51:11 +0100

    Ejercicio JS 13 Error

--- Ejercicio_JS/js/error13.js
+++ Ejercicio_JS/js/error13.js
@@ -1,6 +1,8 @@
-try {
-    throw new EvalError('Se ha producido un error, ejercicio13.js', 100);
-}
-catch(err) {
-    console.log(err.message);
+function error(){
+    try {
+        aleeeeert("Alertita");
+    }
+    catch(error) {
+        throw new EvalError('Se ha producido un error, error13.js', 100);
+    }
 }
