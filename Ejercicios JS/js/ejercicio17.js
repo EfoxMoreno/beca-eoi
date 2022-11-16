a3722804192c8329f38180a4f123e024b02bed99
Angel <efox.host@gmail.com>
2022-11-16 16:57:54 +0100

    Ejercicio 17 JS

---
+++ Ejercicios JS/js/ejercicio17.js
@@ -0,0 +1,5 @@
+console.log("href: " + location.href);
+console.log("protoccol: " + location.protoccol);
+console.log("host: " + location.host);
+console.log("pathname: " + location.pathname);
+console.log("search: " + location.search);
