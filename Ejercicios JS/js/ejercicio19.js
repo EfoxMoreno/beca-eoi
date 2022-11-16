2f307a1450c4b87f9a91cb628b8c60c9a63ba28e
Angel <efox.host@gmail.com>
2022-11-16 17:38:03 +0100

    Ejercicio 19 JS

---
+++ Ejercicios JS/js/ejercicio19.js
@@ -0,0 +1,11 @@
+var elementTagName = document.getElementsByTagName("header");
+console.log(elementTagName);
+
+var elementById = document.getElementById("cabecera");
+console.log(elementById);
+
+var elementQuery = document.querySelector("header");
+console.log(elementQuery);
+
+var elementQueryId = document.querySelector("#cabecera");
+console.log(elementQueryId);
