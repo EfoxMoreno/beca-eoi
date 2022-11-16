f715b4ef9c80780fd9d91926ba467f4f33de013b
Angel <efox.host@gmail.com>
2022-11-16 15:38:23 +0100

    Ejercicio 14 JS

---
+++ Ejercicios JS/js/ejercicio14_Objetos.js
@@ -0,0 +1,39 @@
+/* Creamos objetoPadre */
+var objetoPadre={
+    edad:null,
+    nombre:null,
+    apellidos:null,
+    muestraDatos:function(){
+    console.log(this.nombre + ' ' + this.apellidos + ' tiene una edad de ' + this.edad);
+    }
+};
+
+// Resultado: Null a todo
+console.log(objetoPadre);
+
+// Creamos hijo1 a partir de objetoPadre
+var hijo1 = Object.create(objetoPadre);
+hijo1.edad = 21;
+hijo1.nombre = 'Antonio';
+hijo1.apellidos = 'Martinez Garcia';
+
+console.log(hijo1);
+
+// Creamos hijo2 a partir de objetoPadre
+var hijo2=Object.create(objetoPadre);
+hijo2.edad=30;
+hijo2.nombre='Pepe';
+hijo2.apellidos='Martinez Garcia';
+
+console.log(hijo2);
+
+// Mostramos los datos completos de cada hijo
+hijo1.muestraDatos();
+hijo2.muestraDatos();
+
+// Creamos una función nueva del Objeto Padre que será heredada por hijo1 e hijo2 a través del prototype
+objetoPadre['verEdadHijos'] = function(){
+    console.log(this.edad);
+};
+hijo1.verEdadHijos();
+hijo2.verEdadHijos();
