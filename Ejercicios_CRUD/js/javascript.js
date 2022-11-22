02aec420f189282f938d656d1eedb9ff18954577
edusg83 <edusg83@gmail.com>
2022-11-22 19:10:49 +0100

    borrado - modal

--- Ejercicios_CRUD/js/javascript.js
+++ Ejercicios_CRUD/js/javascript.js
@@ -1,3 +1,4 @@
+var idItemEliminar;
 const headers = {
     'Content-Type':'application/json',
     'Access-Control-Allow-Origin':'*'
@@ -29,7 +30,7 @@ axios.get(urlUsers,{headers})
             <td class="col-sm-3 p-2 mb-2 border border-2 border-danger">${item.description}</td>
             <td class="col-sm-4 p-2 mb-2 border border-2 border-danger">${item.code}</td>
             <td class="col-sm-4 p-2 mb-2 border border-2 border-danger">
-                <button data-bs-toggle='modal' data-bs-target='#modalDelete' onclick="eliminarObjeto(${item.id})">
+                <button data-bs-toggle='modal' data-bs-target='#modalDelete' onclick="guardaIdEliminar(${item.id}, '${item.name}')">
                     <svg xmlns="http://www.w3.org/2000/svg" class="iconClick iconDeleteClick" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16">
                         <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z"/>
                     </svg>
@@ -78,8 +79,13 @@ function editarObjeto(idItem) {
     });
 }
 
-function eliminarObjeto(idItem){    
-    axios.delete(urlUsers+"/"+idItem,{headers})
+function guardaIdEliminar(idItem, nombreProducto){
+    idItemEliminar=idItem;
+    document.getElementById("nombreProducto").innerHTML=nombreProducto;
+}
+
+function eliminarObjeto(){    
+    axios.delete(urlUsers+"/"+idItemEliminar,{headers})
     .then((respuesta) => {
         console.log(respuesta.data);
         window.location.assign("index.html");
