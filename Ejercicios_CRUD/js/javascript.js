930839630e6e79c42374c4c9629872e43c7e8a15
Angel <efox.host@gmail.com>
2022-11-22 17:04:03 +0100

    Form

--- Ejercicios_CRUD/js/javascript.js
+++ Ejercicios_CRUD/js/javascript.js
@@ -1,4 +1,4 @@
-    const headers = {
+const headers = {
     'Content-Type':'application/json',
     'Access-Control-Allow-Origin':'*'
 };
@@ -14,6 +14,7 @@ axios.get(urlUsers,{headers})
             <th class="p-2 mb-2 border border-2 border-dark">Name</th>
             <th class="p-2 mb-2 border border-2 border-dark">Description</th>
             <th class="p-2 mb-2 border border-2 border-dark">Code</th>
+            <th class="p-2 mb-2 border border-2 border-dark">Options</th>
         </tr>
     </thead>
     <tbody>`;
@@ -27,8 +28,46 @@ axios.get(urlUsers,{headers})
             <td class="col-sm-3 p-2 mb-2 border border-2 border-danger">${item.name}</td>
             <td class="col-sm-3 p-2 mb-2 border border-2 border-danger">${item.description}</td>
             <td class="col-sm-4 p-2 mb-2 border border-2 border-danger">${item.code}</td>
+            <td class="col-sm-4 p-2 mb-2 border border-2 border-danger">
+                <button data-bs-toggle='modal' data-bs-target='#exampleModal' onclick="identificador(${item.id})">Edit</button> 
+            </td>
         </tr>`;
     });
     tabla += filas+finTabla;
     document.getElementById("resultados").innerHTML = tabla;
 });
+
+
+// Nuevo Ejercicio 22/11/2022
+
+function identificador(idItem) {
+    axios.get(urlUsers,{headers})
+    .then((respuestaProductos) => {
+        respuestaProductos.data.forEach(item => {
+            if(item.id === idItem){
+                document.getElementById("recipient-id").value = item.id;
+                document.getElementById("recipient-name").value = item.name;
+                document.getElementById("message-text").value = item.description;
+                document.getElementById("recipient-code").value = item.code;
+                
+                // Formulario para modificar articulos
+                const form = document.getElementById('formulario');
+                form.addEventListener('submit', function(element) {
+                    element.preventDefault();
+                    const formData = new FormData(form);
+                    const dataRequest = {
+                        "id":item.id,
+                        "name":item.name,
+                        "description":item.description,
+                        "code":item.code
+                    };
+                    axios.post(urlUsers,dataRequest,{headers})
+                    .then((url) => {
+                        window.location.assign("index.html");
+                    });
+                });
+            }
+        });
+    });
+}
+
